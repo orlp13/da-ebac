ceed3623593944a938e46970d9a81ed6a0fb5692
orlp13 <orlp1329624@gmail.com>
2024-10-14 21:23:41 +0000

    Atualização do gráfico de linha da gasolina

--- gasolina.py
+++ gasolina.py
@@ -4,15 +4,14 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Carrega os dados do arquivo CSV
-data = pd.read_csv('gasolina.csv')
+df_gasolina = pd.read_csv('gasolina.csv')
 
 # Cria o gráfico de linha
 plt.figure(figsize=(10, 6))
-sns.lineplot(x='dia', y='venda', data=data)
-plt.xlabel('Dia')
-plt.ylabel('Preço da Gasolina')
+sns.lineplot(x='dia', y='venda', data=df_gasolina)
 plt.title('Preço Médio da Gasolina em São Paulo (Julho/2021)')
+plt.xlabel('Dia')
+plt.ylabel('Preço (R$)')
 plt.savefig('gasolina.png')
-
 plt.show()
-
+    
